8bcdfc7dded72217d32110c32646818e6d61840d
Jeff Shen <jshen2014@hotmail.com>
2020-10-31 23:40:12 -0400

    add partial derivative function

--- src/optimize/gradient.rs
+++ src/optimize/gradient.rs
@@ -47,3 +47,48 @@ where
     let h = calc_h(x);
     (f(x + h) - f(x)) / h
 }
+
+/// Calculates the partial derivative of a function `f` with respect to the `i`th variable, where
+/// `x` are the variables.
+///
+/// ```rust
+/// use compute::optimize::partial;
+/// use approx_eq::assert_approx_eq;
+///
+/// fn func1(vars: &[f64]) -> f64 {
+///     vars[0].powi(2) + vars[0] * vars[1] + vars[1].powi(2)
+/// }
+///
+/// // partial of x^2 + xy + y^2 at (1, 1)
+/// assert_approx_eq!(partial(func1, &[1., 1.], 0), 3.); // wrt x
+/// assert_approx_eq!(partial(func1, &[1., 1.], 1), 3.); // wrt y
+///
+/// fn func2(vars: &[f64]) -> f64 {
+///     vars[0].powi(2) * vars[1].powi(3)
+/// }
+///
+/// // partial of x^2 * y^3
+/// assert_approx_eq!(partial(func2, &[5., 1.2], 0), 2. * 5. * 1.2_f64.powi(3));
+/// assert_approx_eq!(partial(func2, &[0.1, -2.], 1), 3. * (-2. as f64).powi(2) * (0.1_f64).powi(2));
+///
+/// fn func3(vars: &[f64]) -> f64 {
+///     (vars[0].sin() / vars[1].exp()).powf(vars[1])
+/// }
+///
+/// // partial of (sin(x) / e^y)^y with respect to x at (5, 5)
+/// assert_approx_eq!(partial(func3, &[5., 5.], 0), 1.665507727894749327e-11);
+///
+/// // partial of a closure
+/// assert_approx_eq!(partial(|x: &[f64]| x[0] * x[0] + 2., &[0.], 0), 0.);
+/// ```
+pub fn partial<F>(f: F, x: &[f64], i: usize) -> f64
+where
+    F: Fn(&[f64]) -> f64,
+{
+    let h = calc_h(x[i]);
+    let mut xph = x.to_owned();
+    xph[i] += h;
+    let mut xmh = x.to_owned();
+    xmh[i] -= h;
+    (f(&xph) - f(&xmh)) / (2. * h)
+}

240401951b1c013a17181435682848f1e85959a5
Jeff Shen <jshen2014@hotmail.com>
2021-01-28 01:17:14 -0500

    delete old ndarray methods

--- src/distributions/mod.rs
+++ src/distributions/mod.rs
@@ -13,7 +13,6 @@ mod pareto;
 mod poisson;
 mod t;
 mod uniform;
-// use ndarray::{Array, Ix1, Ix2};
 
 /// The primary trait defining a probability distribution.
 pub trait Distribution: Send + Sync {
@@ -23,14 +22,7 @@ pub trait Distribution: Send + Sync {
     fn sample_vec(&self, n: usize) -> Vec<f64> {
         (0..n).map(|_| self.sample()).collect()
     }
-    // /// Creates an 1d array with values sampled from the given distribution.
-    // fn vector(&self, shape: usize) -> Array<f64, Ix1> {
-    //     Array::from_shape_fn(shape, |_| self.sample())
-    // }
-    // /// Creates an 2d matrix with values sampled from the given distribution.
-    // fn matrix(&self, shape: (usize, usize)) -> Array<f64, Ix2> {
-    //     Array::from_shape_fn(shape, |_| self.sample())
-    // }
+
     fn update(&mut self, params: &[f64]);
 }
 

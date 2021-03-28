83c3db2c833d4d46b01182f7e3b94210399fa4e2
Jeff Shen <jshen2014@hotmail.com>
2021-03-27 23:07:24 -0400

    make cw and ccw rotation matrices for clarity

--- src/linalg/rotations.rs
+++ src/linalg/rotations.rs
@@ -5,10 +5,58 @@ pub enum Axis {
     Y,
     Z,
 }
-
-/// Returns a 3D rotation matrix along the `axis` axis (either X, Y, or Z) with a given angle in
+///
+/// Returns a 3D clockwise rotation matrix along the `axis` axis (either X, Y, or Z) with a given angle in
 /// radians.
-pub fn rotation_matrix(angle: f64, axis: Axis) -> Vec<f64> {
+///
+/// # Remarks
+/// These are clockwise rotation matrices. Use `rotation_matrix_ccw` to get counter-clockwise rotation
+/// matrices.
+pub fn rotation_matrix_cw(angle: f64, axis: Axis) -> Vec<f64> {
+    match axis {
+        Axis::X => vec![
+            1.,
+            0.,
+            0.,
+            0.,
+            angle.cos(),
+            angle.sin(),
+            0.,
+            -angle.sin(),
+            angle.cos(),
+        ],
+        Axis::Y => vec![
+            angle.cos(),
+            0.,
+            -angle.sin(),
+            0.,
+            1.,
+            0.,
+            angle.sin(),
+            0.,
+            angle.cos(),
+        ],
+        Axis::Z => vec![
+            angle.cos(),
+            angle.sin(),
+            0.,
+            -angle.sin(),
+            angle.cos(),
+            0.,
+            0.,
+            0.,
+            1.,
+        ],
+    }
+}
+
+/// Returns a 3D counter-clockwise rotation matrix along the `axis` axis (either X, Y, or Z) with a given angle in
+/// radians.
+///
+/// # Remarks
+/// These are counter-clockwise rotation matrices. Use `rotation_matrix_cw` to get clockwise rotation
+/// matrices.
+pub fn rotation_matrix_ccw(angle: f64, axis: Axis) -> Vec<f64> {
     match axis {
         Axis::X => vec![
             1.,
@@ -19,7 +67,7 @@ pub fn rotation_matrix(angle: f64, axis: Axis) -> Vec<f64> {
             -angle.sin(),
             0.,
             angle.sin(),
-            -angle.cos(),
+            angle.cos(),
         ],
         Axis::Y => vec![
             angle.cos(),

f902caf60d41ac52447b625eb0f7b79b76ae1b20
Jeff Shen <jshen2014@hotmail.com>
2021-06-05 17:06:41 -0400

    implement vector operations on matrix

--- src/linalg/vec/matrix.rs
+++ src/linalg/vec/matrix.rs
@@ -1,11 +1,10 @@
 use std::{
-    array::IntoIter,
     fmt::{Display, Formatter, Result},
-    iter::Map,
     ops::Index,
-    ops::Range,
-    slice::Chunks,
 };
+
+use impl_ops::*;
+use std::ops;
 
 use crate::prelude::{is_square, is_symmetric};
 
@@ -58,8 +57,29 @@ impl Matrix {
         }
     }
 
-    pub fn row(&self, row: usize) -> Vector {
+    pub fn shape(&self) -> [usize; 2] {
+        [self.nrows, self.ncols]
+    }
+
+    pub fn size(&self) -> usize {
+        self.nrows * self.ncols
+    }
+
+    pub fn get_row(&self, row: usize) -> Vector {
+        assert!(row < self.nrows);
         Vector::from(&self[row])
+    }
+
+    pub fn get_col(&self, col: usize) -> Vector {
+        assert!(col < self.ncols);
+
+        let mut v = vec![0.; self.nrows];
+
+        for i in 0..self.nrows {
+            v[i] = self[i][col];
+        }
+
+        Vector::from(v)
     }
 }
 
@@ -124,6 +144,7 @@ impl Index<usize> for Matrix {
     type Output = [f64];
     fn index(&self, i: usize) -> &Self::Output {
         assert!(i < self.nrows);
+        assert!((i + 1) * self.ncols < self.data.len());
         &self.data[i * self.ncols..(i + 1) * self.ncols]
     }
 }
@@ -136,3 +157,99 @@ impl Index<[usize; 2]> for Matrix {
         &self.data[i * self.ncols + j]
     }
 }
+
+// vector-vector ops
+macro_rules! impl_vv_ops_helper {
+    ($op: tt) => {
+        impl_op_ex!($op |u: &Matrix, v: &Matrix| -> Matrix {
+            assert_eq!(u.shape(), v.shape());
+            Matrix::new( &u.data $op &v.data, u.shape())
+        });
+    };
+}
+
+impl_vv_ops_helper!(+);
+impl_vv_ops_helper!(-);
+impl_vv_ops_helper!(*);
+impl_vv_ops_helper!(/);
+
+// vector-float and float-vector ops
+impl_op_ex_commutative!(+ |f: f64, v: &Matrix| -> Matrix { Matrix::new(&v.data + f, v.shape()) });
+impl_op_ex_commutative!(*|f: f64, v: &Matrix| -> Matrix { Matrix::new(&v.data * f, v.shape()) });
+impl_op_ex!(-|f: f64, v: &Matrix| -> Matrix { Matrix::new(f - &v.data, v.shape()) });
+impl_op_ex!(-|v: &Matrix, f: f64| -> Matrix { Matrix::new(&v.data - f, v.shape()) });
+impl_op_ex!(/|f: f64, v: &Matrix| -> Matrix { Matrix::new(f / &v.data, v.shape()) });
+impl_op_ex!(/|v: &Matrix, f: f64| -> Matrix { Matrix::new(&v.data / f, v.shape()) });
+
+macro_rules! impl_unaryops_matrix {
+    ($op: ident) => {
+        impl Matrix {
+            #[doc = "Apply the `f64` operation `"]
+            #[doc = stringify!($op)]
+            #[doc = "` element-wise to the matrix."]
+            pub fn $op(&self) -> Self {
+                Self::new(self.data.$op(), self.shape())
+            }
+        }
+    };
+}
+
+impl_unaryops_matrix!(ln);
+impl_unaryops_matrix!(ln_1p);
+impl_unaryops_matrix!(log10);
+impl_unaryops_matrix!(log2);
+impl_unaryops_matrix!(exp);
+impl_unaryops_matrix!(exp2);
+impl_unaryops_matrix!(exp_m1);
+impl_unaryops_matrix!(sin);
+impl_unaryops_matrix!(cos);
+impl_unaryops_matrix!(tan);
+impl_unaryops_matrix!(sinh);
+impl_unaryops_matrix!(cosh);
+impl_unaryops_matrix!(tanh);
+impl_unaryops_matrix!(asin);
+impl_unaryops_matrix!(acos);
+impl_unaryops_matrix!(atan);
+impl_unaryops_matrix!(asinh);
+impl_unaryops_matrix!(acosh);
+impl_unaryops_matrix!(atanh);
+impl_unaryops_matrix!(sqrt);
+impl_unaryops_matrix!(cbrt);
+impl_unaryops_matrix!(abs);
+impl_unaryops_matrix!(floor);
+impl_unaryops_matrix!(ceil);
+impl_unaryops_matrix!(to_radians);
+impl_unaryops_matrix!(to_degrees);
+impl_unaryops_matrix!(recip);
+impl_unaryops_matrix!(round);
+impl_unaryops_matrix!(signum);
+
+macro_rules! impl_unaryops_with_arg_matrix {
+    ($op: ident, $argtype: ident) => {
+        impl Matrix {
+            #[doc = "Apply the `f64` operation `"]
+            #[doc = stringify!($op)]
+            #[doc = "` element-wise to the matrix."]
+            pub fn $op(&self, arg: $argtype) -> Self {
+                Self::new(self.data.$op(arg), self.shape())
+            }
+        }
+    };
+}
+
+impl_unaryops_with_arg_matrix!(powi, i32);
+impl_unaryops_with_arg_matrix!(powf, f64);
+
+macro_rules! impl_reduction_fns_matrix {
+    ($($fn: ident),+) => {
+        impl Matrix {
+            $(
+                pub fn $fn(&self) -> f64 {
+                    self.data.$fn()
+                }
+            )+
+        }
+    };
+}
+
+impl_reduction_fns_matrix!(norm, max, mean, min, std, sum, var, sample_std, sample_var);

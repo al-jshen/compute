acfb1558d523530eb81bd842fb6633248cc426ff
Jeff Shen <jshen2014@hotmail.com>
2021-09-07 22:32:46 -0400

    comment out use arch line

--- src/linalg/array/vops.rs
+++ src/linalg/array/vops.rs
@@ -1,5 +1,5 @@
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-use std::arch::x86_64::*;
+// #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+// use std::arch::x86_64::*;
 
 // /// Vector-vector operations.
 // #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
